c9683d8bf56d5a920b34be694ebd875f59520d6c
rht <rhtbot@protonmail.com>
2019-01-24 17:00:37 +0000

    my_library_unittests: Fix gcc path

--- my_library_unittests/unittests/compile_unittest_library.bat
+++ my_library_unittests/unittests/compile_unittest_library.bat
@@ -1,1 +1,1 @@
-gcc -D_DEBUG_MODE -g  ../my_library_functions.c ../ unittest_suite_my_library.c unittests_my_library.c -lm -lcunit -lgsl -lgslcblas
+gcc -D_DEBUG_MODE -g  ../my_library_functions.c ../ unittest_suite_library.c unittests_my_library.c -lm -lcunit -lgsl -lgslcblas
